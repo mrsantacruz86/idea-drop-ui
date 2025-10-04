316fb08b40e25a739fb13a1b4c12969ab9122dcc
Nelson Diaz <abnerdc86@gmail.com>
2025-10-03 21:01:29 -0400

    Move fetchIdea method to the api ideas.ts file

--- src/api/ideas.ts
+++ src/api/ideas.ts
@@ -5,3 +5,8 @@ export const fetchIdeas = async (): Promise<Idea[]> => {
   const res = await api.get('/ideas');
   return res.data;
 };
+
+export const fetchIdea = async (ideaId: string): Promise<Idea> => {
+  const res = await api.get(`/ideas/${ideaId}`);
+  return res.data;
+};
--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -1,13 +1,7 @@
 import { createFileRoute, Link } from '@tanstack/react-router';
 import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
-import type { Idea } from '@/types';
-import api from '@/lib/axios';
 import { ArrowLeft } from 'lucide-react';
-
-const fetchIdea = async (ideaId: string): Promise<Idea> => {
-  const res = await api.get(`/ideas/${ideaId}`);
-  return res.data;
-};
+import { fetchIdea } from '@/api/ideas';
 
 const ideaQueryOptions = (ideaId: string) =>
   queryOptions({
