e22d081226f5d62b89feb47456f1a04d47ece4ed
Nelson Diaz <abnerdc86@gmail.com>
2025-10-29 23:07:32 -0400

    Auth Context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,29 @@
+import { type ReactNode, createContext, useContext, useState } from 'react';
+
+type AuthContextType = {
+  accessToken: string | null;
+  setAccessToken: (token: string) => void;
+  user: { id: string; email: string; name: string } | null;
+  setUser: (user: AuthContextType['user']) => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [user, setUser] = useState<AuthContextType['user']>(null);
+
+  return (
+    <AuthContext.Provider
+      value={{ accessToken, setAccessToken, user, setUser }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const ctx = useContext(AuthContext);
+  if (!ctx) throw new Error('useAuth must be used within AuthProvider');
+  return ctx;
+};
