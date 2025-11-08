d0f5ef8eabf347f267c9d50dcc73b74b70855558
Nelson Diaz <abnerdc86@gmail.com>
2025-11-08 01:27:05 -0500

    Refresh Token On Page Refresh

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,11 @@
-import { type ReactNode, createContext, useContext, useState } from 'react';
+import {
+  type ReactNode,
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+} from 'react';
+import { refreshAccessToken } from '@/api/auth';
 
 type AuthContextType = {
   accessToken: string | null;
@@ -12,6 +19,19 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [accessToken, setAccessToken] = useState<string | null>(null);
   const [user, setUser] = useState<AuthContextType['user']>(null);
+
+  useEffect(() => {
+    const loadAuth = async () => {
+      try {
+        const { accessToken: newToken, user } = await refreshAccessToken();
+        setAccessToken(newToken);
+        setUser(user);
+      } catch (err: any) {
+        console.error('Failed to refresh token', err);
+      }
+    };
+    loadAuth();
+  }, []);
 
   return (
     <AuthContext.Provider
